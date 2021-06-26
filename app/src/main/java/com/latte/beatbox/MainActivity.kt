8c05eb02079133a11331b09837aca060d04c68db
dev21 <13467987@naver.com>
2021-06-27 04:53:48 +0900

    push test

--- app/src/main/java/com/latte/beatbox/MainActivity.kt
+++ app/src/main/java/com/latte/beatbox/MainActivity.kt
@@ -11,6 +11,7 @@ import com.latte.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // --
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
